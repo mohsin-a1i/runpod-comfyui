bf8fc0f5da1b123eb7a4c01d6401c0cbca7eb96a
mohsin-a1i <mohsin_ali96@hotmail.com>
2026-01-21 07:31:41 +0500

    Removed high vram flag

--- backend/start.sh
+++ backend/start.sh
@@ -26,7 +26,7 @@ if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "worker-comfyui: Starting RunPod Handler"
     python -u handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u comfyui/main.py --highvram \
+    python -u comfyui/main.py \
         --use-sage-attention \
         --disable-auto-launch \
         --disable-metadata \
